4f95e1755d120a559a9c8a5d0f1bd1d8e2f976e5
taniyukiya <taniyky@sohobb.jp>
2021-04-16 10:13:13 +0900

    readjson

--- readJson.js
+++ readJson.js
@@ -1,33 +1,40 @@
-const fs =require('fs');
-    try{
-        const existance = fs.statSync('countries-50m.json');
-        //console.log(existance);
-    }
-    catch(err){
-        console.log(error)
-    }
+const fs = require("fs");
+try {
+  const existance = fs.statSync("countries-50m.json");
+  //console.log(existance);
+} catch (err) {
+  console.log(err);
+}
 
-    try{
-        const countryData =fs.readFileSync('countries-50m.json');
-        let resultList = [];
-        var obj = JSON.parse(countryData);
-        //var jsonCount = Object.keys(obj).length; 普通の連想配列ならこれでOKだけどネストなので無力
-        function getValue(obj) {
-            if(typeof obj === "object"){
-                var strValue='';
-                for(key in obj){
-                   if(key=="name"){
-                        resultList.push();
-                   }                 
-                }
-                
-            }
+try {
+  const countryData = fs.readFileSync("countries-50m.json");
+  let resultList = [];
+  const Jsonobj = JSON.parse(countryData);
+  //var jsonCount = Object.keys(obj).length; 普通の連想配列ならこれでOKだけどネストなので無力
+  function getValue(obj) {
+    if (typeof obj === "object") {
+      let strValue = "";
+        for (key in obj) {
+            if (key === "name") {
+          resultList.push(obj[key]);
+             }
+            strValue += getValue(obj[key]);
         }
-
-        }
-
-
-    
-    catch(err){
-        console.log(error)
+      return strValue;
+    } else {
+      return obj;
     }
+  }
+  getValue(Jsonobj);
+  //console.log(getValue(Jsonobj));
+  resultList = JSON.stringify(resultList);
+  fs.writeFile("./result.json", resultList, "utf-8", (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  //console.log(resultList);
+  //console.log(resultList.length);
+} catch (e) {
+  console.log(e);
+}
